e7f9f5695e27485ecb312c8e9a3b9127065c6dce
julibucci <buccijulieta0@gmail.com>
2024-08-06 21:24:22 -0300

    insertion sort code

--- OneDrive/Escritorio/Exercises - Solvd/Exercise1- solvd/src/Main.java
+++ OneDrive/Escritorio/Exercises - Solvd/Exercise1- solvd/src/Main.java
@@ -2,14 +2,38 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        // Declare and initialize the array
+        int[] array = {12, 11, 13, 5, 6};
+
+        insertionSort(array);
+
+        // Print the sorted array
+        for (int i = 0; i < array.length; i++)
+        {
+            System.out.print(array[i] + " ");
+        }
+
+    }
+
+    public static void insertionSort(int[] array)
+    {
+        // Loop through the array starting from the second element to the end
+        for (int i = 1; i < array.length; i++)
+        {
+            int t = array[i]; // t is the actual element
+            int j = i - 1; // j is the index of the element before the current one
+
+            // Move elements of array that are greater than t, one position ahead to make space for the t
+            while (j >= 0 && array[j] > t)
+            {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+
+            array[j + 1] = t; // Insert the t in its correct position
         }
     }
 }
+
+
